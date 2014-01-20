0b254af046bee129aeb713a231a2a252b8ca2b09
mwitkows <mwitkows@stevens.edu>
2014-01-20 15:37:34 -0500

    Fixed roman roulette, so it works now. Problem with c++ iterators being invalidated.

--- uva/130/c++/roulette.cpp
+++ uva/130/c++/roulette.cpp
@@ -1,23 +1,31 @@
 #include "roulette.h"
+#include <iostream>
 
 uint roulette(uint people, uint step)
 {
 	std::list<uint>::iterator iterator;
-	
 	for(uint i = people; i > 0; i--)
 		gl_myList.push_front(i);
 
 	iterator = gl_myList.begin();
-
 	while(people > 1)
 	{
-		for(uint i = 0; i < step; i++)
+		iterator = advanceList(iterator, step - 1);
+		uint replaceValue = *iterator; // Node n's value
+		auto tempIterator = advanceList(iterator, step);
+		uint newValue = *tempIterator; //Node n + step value
+
+		*iterator = *tempIterator;
+		iterator = gl_myList.erase(tempIterator);
+			
+		//Puts the iterator back to the correct spot, and sets it value
+		while(*iterator != newValue && gl_myList.size() > 1)
 		{
-			iterator++;
-			if(iterator == gl_myList.end())
-				iterator = gl_myList.begin();
-			*iterator = kill(iterator, step);
+			iterator = advanceList(iterator, 1);
+
 		}
+		
+		iterator = advanceList(iterator, 1);
 		people--;
 	}
 
--- uva/130/c++/roulette.h
+++ uva/130/c++/roulette.h
@@ -6,5 +6,6 @@
 	typedef unsigned int uint;
 	static std::list<uint> gl_myList;
 	uint roulette(uint people, uint step);
-	uint kill(std::list<uint>::iterator iter, uint step);
+	void kill(std::list<uint>::iterator& iterator, uint step);
+	std::list<uint>::iterator advanceList(std::list<uint>::iterator start, uint step);
 #endif
