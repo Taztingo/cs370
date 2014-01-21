2f87a8b3c32e7a301b988fe18f4c2a14c5cda549
mwitkows <mwitkows@stevens.edu>
2014-01-21 01:44:11 -0500

    Added Makefile to problem 123, and created basic main for problem.

---
+++ uva/123/c++/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+int main()
+{
+	std::map<std::string, int> exclusions;
+	std::vector<std::string> titles;
+
+	exclusions["is"] = 1;
+	exclusions["the"] = 1;
+	exclusions["of"] = 1;
+	exclusions["and"] = 1;
+	exclusions["a"] = 1;
+	exclusions["but"] = 1;
+	exclusions["as"] = 1;
+
+	titles.push_back("Descent of Man");
+	titles.push_back("The Ascent of Man");
+	titles.push_back("The Old Man and The Sea");
+	titles.push_back("A Portrait of the Artist As a Young Man");
+	titles.push_back("A Man is a Man but Bubblesort IS A DOG");
+
+	return 0;
+}
