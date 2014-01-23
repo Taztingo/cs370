61651fb98710690181d3b788a212b2b5daca6b8a
mwitkows <mwitkows@stevens.edu>
2014-01-23 17:08:42 -0500

    Added semi working createKwics.

--- uva/123/c++/createKwics.cpp
+++ uva/123/c++/createKwics.cpp
@@ -2,8 +2,32 @@
 #include "Kwic.h"
 #include <vector>
 #include <unordered_map>
+#include <cstring>
+#include <string>
+#include <iostream>
 
 void createKwics(const std::unordered_map<std::string, int>& exclusions, const std::string& title, std::vector<Kwic>& kwics)
 {
+	char* titleString = new char[title.size() + 1];
+	int i = 0;
+	for(auto iterator = title.begin(); iterator != title.end(); i++, iterator++)
+	{
+		titleString[i] = *iterator;
+	}
+	titleString[i] = '\0';
 	
+	char* token = strtok(titleString, " ");
+	do
+	{
+		std::string tokenString(token);
+		if(!isExclusion(exclusions, tokenString))
+		{
+			Kwic kwic(token, title);
+			kwics.push_back(kwic);
+		}
+		//free(token);
+	}
+	while(token = strtok(NULL, " "));
+
+	delete[] titleString;
 }
--- uva/123/c++/main.cpp
+++ uva/123/c++/main.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
+#include "Kwic.h"
 
 int main()
 {
 	std::unordered_map<std::string, int> exclusions;
 	std::vector<std::string> titles;
+	std::vector<Kwic> kwics;
 	
 	exclusions["is"] = 1;
 	exclusions["the"] = 1;
@@ -17,12 +19,23 @@ int main()
 	exclusions["but"] = 1;
 	exclusions["as"] = 1;
 
-	titles.push_back("Descent of Man");
-	titles.push_back("The Ascent of Man");
-	titles.push_back("The Old Man and The Sea");
-	titles.push_back("A Portrait of the Artist As a Young Man");
-	titles.push_back("A Man is a Man but Bubblesort IS A DOG");
+	titles.push_back("descent of man");
+	titles.push_back("the ascent of man");
+	titles.push_back("the old man and the sea");
+	titles.push_back("a portrait of the artist as a young man");
+	titles.push_back("a man is a man but bubblesort is a dog");
 	std::sort(titles.begin(), titles.end());
+
+	for(auto i = 0; i < titles.size(); i++)
+	{
+		createKwics(exclusions, titles[i], kwics);
+	}
+	std::make_heap(kwics.begin(), kwics.end());
+
+	for(auto i = kwics.begin(); i != kwics.end(); i++)
+	{
+		std::cout << i->getTitle() << " / Keyword: " << i->getKeyword() << "\n";
+	}
 
 	return 0;
 }
