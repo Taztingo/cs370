0dbba63da3d52fbe5fa7ec542a3d6f31da545c1e
mwitkows <mwitkows@stevens.edu>
2014-01-23 14:15:57 -0500

    Added createKwics to create all Kwics for a word, and add them to kwic vector.

---
+++ uva/123/c++/createKwics.cpp
@@ -0,0 +1,9 @@
+#include "titles.h"
+#include "Kwic.h"
+#include <vector>
+#include <unordered_map>
+
+void createKwics(const std::unordered_map<std::string, int>& exclusions, const std::string& title, std::vector<Kwic>& kwics)
+{
+	
+}
--- uva/123/c++/titles.h
+++ uva/123/c++/titles.h
@@ -2,7 +2,10 @@
 	#define TITLES_H_
 
 	#include <unordered_map>
+	#include <vector>
 	#include <string>
+	#include "Kwic.h"
 	bool isExclusion(const std::unordered_map<std::string, int>& map, const std::string& word);
+	void createKwics(const std::unordered_map<std::string, int>& exclusions, const std::string& title, std::vector<Kwic>& kwics);
 
 #endif
