7fba78271a7eb66e1554c01483f4df90f437e21a
mwitkows <mwitkows@stevens.edu>
2014-01-29 14:18:24 -0500

    Added comments to roulette so we discuss what is happening within the code.

--- uva/130/c++/roulette.cpp
+++ uva/130/c++/roulette.cpp
@@ -7,18 +7,25 @@ uint roulette(uint people, uint step)
 	uint position = 0;
 	uint digger = 0;
 	
+	//Add everyone to circle
 	for(uint i = 1; i <= people; i++)
 		array.push_back(i);
 	
+	//Loop until one person is left
 	while(array.size() > 1)
 	{
+		//Find next person to kill
 		if(people == array.size())
 			position = (position + step - 1) % array.size();
 		else
 			position = (position + step) % array.size();
+
+		//Dig the grave
 		digger = (position + step) % array.size();	
 		array[position] = array[digger];
 		array.erase(array.begin() + digger);
+
+		//Fix index out of bounds when person
 		if(position == array.size())
 			position--;
 		if(digger == 0)
