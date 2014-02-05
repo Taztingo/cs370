15cccdc57e89de86ff4a4bd9df49e88b11c4cd08
mwitkows <mwitkows@stevens.edu>
2014-02-04 22:40:00 -0500

    Added skeleton methods for ClosestPair.h, and also added getDistance method.

--- hacker/closestNumbers/c++/ClosestPair.h
+++ hacker/closestNumbers/c++/ClosestPair.h
@@ -10,10 +10,32 @@
 				long _distance;
 				std::vector<int> _list;
 				void clear();
+				long getDistance(int number1, int number2);
 			
 			public:
 				ClosestPair();
 				~ClosestPair();
 				void addPair(int number1, int number2);
 	};
+
+	ClosestPair::ClosestPair()
+	{
+	}
+
+	ClosestPair::~ClosestPair()
+	{
+	}
+
+	void ClosestPair::clear()
+	{
+	}
+
+	void addPair(int number1, int number2)
+	{
+	}
+
+	long getDistance(int number1, int number2)
+	{
+		return 0;
+	}
 #endif
