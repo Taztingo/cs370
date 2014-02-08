d70d663d4fe3895393320a8b9c102da938d3ff3a
mwitkows <mwitkows@stevens.edu>
2014-02-08 00:31:43 -0500

    Added submission folder with submission file.

---
+++ hacker/closestNumbers/c++/submission/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+class ClosestPair
+{
+	private:
+		long _distance;
+		const char UNSET = -1;
+		std::vector<int> _list;
+		long getDistance(const int number1, const int number2) const;
+			
+	public:
+		ClosestPair();
+		~ClosestPair();
+		void addPair(const int number1, const int number2);
+		const std::vector<int>& getList() const;
+		const std::string toString() const;
+		void sort();
+
+};
+
+ClosestPair::ClosestPair()
+{
+	_distance = UNSET;
+}
+
+ClosestPair::~ClosestPair()
+{
+}
+
+void ClosestPair::addPair(const int number1, const int number2)
+{
+	long distance = getDistance(number1, number2);
+
+	if(distance < _distance || _distance == UNSET)
+	{
+		_list.clear();
+		_distance = distance;
+		//std::pair<int, int> newPair(number1, number2);
+		//_list.push_back(newPair);
+		_list.push_back(number1);
+		_list.push_back(number2);
+	}
+	else if(distance == _distance)
+	{
+		_list.push_back(number1);
+		_list.push_back(number2);
+		//std::pair<int, int> newPair(number1, number2);
+		//_list.push_back(newPair);
+	}
+}
+
+inline long ClosestPair::getDistance(const int number1, const int number2) const
+{
+	return std::abs(number1 - number2);
+}
+
+const std::vector<int>& ClosestPair::getList() const
+{
+	return _list;
+}
+	
+const std::string ClosestPair::toString() const
+{
+	std::string allPairs;
+
+	for(auto iterator = _list.begin(); iterator != _list.end(); iterator++)
+	{
+		allPairs += std::to_string(*iterator);
+		allPairs += " ";
+		//allPairs += std::to_string(iterator->second);
+		//allPairs += " ";
+	}
+
+	if(allPairs.size() > 0)
+	{
+		allPairs.substr(0, allPairs.size() - 1);
+	}
+
+	return allPairs;
+}
+
+void ClosestPair::sort()
+{
+	std::sort(_list.begin(), _list.end());
+}
+
+void closest(int* array, int size)
+{
+	ClosestPair closestPairs;
+
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = i + 1; j < size; j++)
+		{
+			closestPairs.addPair(array[i], array[j]);
+		}
+	}
+
+	closestPairs.sort();
+	std::cout << closestPairs.toString();
+}
+
+int main()
+{
+	int sizeOfList;
+	std::string allNumbers;
+	std::cin >> sizeOfList >> std::ws;
+	std::getline(std::cin, allNumbers);
+
+	size_t position = 0;
+	size_t nextPosition = 0;
+	int count = 0;
+	int array[sizeOfList];
+	do
+	{
+		position = nextPosition;
+		nextPosition = allNumbers.find(' ', position + 1);
+		if(position > 0)
+			position++;
+		array[count] = atoi(allNumbers.substr(position, nextPosition - position).c_str());
+		count++;
+	}
+	while(nextPosition != std::string::npos);
+
+	closest(array, sizeOfList);
+	return 0;
+}
