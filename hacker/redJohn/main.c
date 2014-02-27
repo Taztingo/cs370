59d3ae4b6e92197ed90e2c21a6d1acd3c3c9bea8
Andrew Cernera <acernera@stevens.edu>
2014-02-27 16:41:31 -0500

    Initial ruby file

--- hacker/redJohn/main.c
+++ hacker/redJohn/main.c
@@ -5,41 +5,6 @@
 
 int sieve(int n)
 {
-	// int results[n+1],
-	// lim,
-	// i,
-	// j;
-
-	// lim = (int) sqrt(n);
-
-	// memset(results, 0, sizeof(int)*(n+1));
-	// results[0] = -1;
-	// results[1] = -1;
-
-	// // printf("Lim:%d\n", lim);
-
-	// for (i = 2; i <= lim; i++)
-	// {
-	// 	// if(results[i] != -1)
-	// 		for(j = i*i; j < n; j+= i)
-	// 		{
-	// 			if(j <= n && j != 2)
-	// 				results[j] = -1;
-	// 		}
-	// }
-
-	// j = 0;
-
-	// // for(i = 0; i < n + 1; i++)
-	// // 	printf("Sieve Results:%d\n", results[i]);
-
-	// for(i = 0; i < n + 1; i++)
-	// 	if(results[i] != -1)
-	// 		j++;
-
-	// return j;
-
-
 	char* sieve;
 	int i, j, m, c;
 
@@ -78,19 +43,13 @@ int main() {
 	
 
 	for(i = 4; i < 41; i++)
-	{
 		bricks[i] = bricks[i-1] + bricks[i-4];
-		// printf("Number of combos at %d: %d\n", i, bricks[i]);
-	}
 
 	scanf("%d", &t);
 
 	for(i = 0; i < t; i ++)
 	{
 		scanf("%d",&n);
-		//sieve(tile(n));
-		// printf("Tile Output:%d\n", tile(n));
-		// printf("Sieve Output:%d\n", sieve(tile(n)));
         printf("%d\n",sieve(bricks[n]));
 	}
 
