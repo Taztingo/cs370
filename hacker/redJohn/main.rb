f0f108cfea3b7dd65a2fb540feaf2ef65e331696
Andrew Cernera <acernera@stevens.edu>
2014-02-28 13:04:58 -0500

    Finished ruby implementation. Its beautiful but inefficient

---
+++ hacker/redJohn/main.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby -w
+
+def sieve_primes(n)
+  num_iter = Integer(Math.sqrt(n)) #Number of times primes array will be sieved
+  primes = (2..n).to_a #Array that will contain primes
+
+  while num_iter > 1 do
+    primes = primes.select do |e| 
+      (((e % num_iter) != 0) or (e == num_iter))
+    end
+    num_iter -= 1
+  end
+
+  return primes
+end
+
+bricks = Array.new(41, 1)
+
+i = 4
+while i < 41 do
+  bricks[i] = bricks[i - 1] + bricks[i - 4]
+  i += 1
+end
+
+n = Integer(gets.chomp)
+
+i = 0
+while i < n
+  puts sieve_primes(bricks[Integer(gets.chomp)]).length
+  i += 1
+end
