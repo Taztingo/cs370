ec9e2e595ced306bf73d300ee04e892c1e347af0
Kooshaba <kooshaba@gmail.com>
2014-03-17 10:52:21 -0400

    Working version of botclean

--- hacker/botClean/bot.rb
+++ hacker/botClean/bot.rb
@@ -1,64 +1,45 @@
 #!/usr/bin/ruby
 
 # Head ends here
-def move_x dist
-    if dist > 0 then
-        return "LEFT"
-    else
-        return "RIGHT"
-    end
-end
 
-def move_y dist
-    if dist > 0 then
-        return "UP"
-    else
-        return "DOWN"
-    end
-end
 
 def next_move posr, posc, board
-    closestY= board.length - 1
-    closestX= board[0].length - 1
-    
-    (0...board.length).each do |i|
-        (0...board[0].length).each do |j|
-            #puts "#{i}, #{j}"
-            puts board[i][j]
-            if board[i][j].eql? 100 then
-                ydist = posr - i
-                xdist = posc - j
+  closest_node = Array.new(3)
+  closest_node[0] = 10
+  closest_node[1] = 10
+  closest_node[2] = closest_node[0] + closest_node[1]
 
-                puts "YDIST:#{ydist}, XDIST:#{xdist}"
-                
-                if ydist.abs < closestY then
-                    closestY = ydist
-                end
-                
-                if xdist.abs < closestX then
-                    closestX = xdist
-                end
-                
-            end
+  (0...5).each do |y|
+    (0...5).each do |x|
+      if board[y][x].eql? "d" then
+        x_dist = posc - x
+        y_dist = posr - y
+
+        #warn "#{x_dist}, #{y_dist}"
+
+        if(closest_node[2] > (x_dist.abs + y_dist.abs)) then
+          closest_node[0] = x_dist
+          closest_node[1] = y_dist
+          closest_node[2] = closest_node[0].abs + closest_node[1].abs
         end
+      end
     end
-    
-    puts "X:#{closestX}, Y:#{closestY}"
-    if closestY == 0 && closestX ==0
-        return "CLEAN"
-    elsif closestY == 0
-        return move_x(closestX)
-    elsif closestX == 0
-        return move_y(closestY)
-    else
-        if closestX.abs < closestY.abs
-            return move_x(closestX)
-        elsif closestY.abs < closestX.abs
-            return move_y(closestY)
-        else
-            return move_y(closestY)
-        end
-    end                           
+  end
+
+  warn "#{closest_node[0]},#{closest_node[1]},#{closest_node[2]}"
+
+  if (closest_node[0] < 0)
+    puts "RIGHT"
+  elsif (closest_node[0] > 0)
+    puts "LEFT"
+  elsif (closest_node[1] < 0)
+    puts "DOWN"
+  elsif (closest_node[1] > 0)
+    puts "UP"
+  else
+    puts"CLEAN"
+  end
+
 end
 
 # Tail starts here
@@ -66,9 +47,9 @@ pos = gets.split.map {|i| i.to_i}
 board = Array.new(5)
 
 (0...5).each do |i|
-    board[i] = gets.strip
+board[i] = gets.strip
 end
 
-puts board
+# puts board
 
 puts next_move pos[0], pos[1], board
