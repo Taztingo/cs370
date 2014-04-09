5f731930f14360f0816687d131f17c0db404d44e
Andy <kooshaba@gmail.com>
2014-04-08 20:01:42 -0400

    started over, wanna go greedy

--- baler/silly_sort/sort.rb
+++ baler/silly_sort/sort.rb
@@ -2,30 +2,13 @@ gets
 
 input = gets.split(" ").map { |x| x.to_i}
 
-swaps = {0 => 0}
 
-def swap(pos1, pos2, nums)
-  nums[pos1] = nums[pos1] ^ nums[pos2]
-  nums[pos2] = nums[pos2] ^ nums[pos1]
-  nums[pos1] = nums[pos1] ^ nums[pos2]
+def sort array
+	if array.length <= 1
+		return array
+	end
 
-  return nums
-   
+	pivot = input.length / 2
+
+	
 end
-
-input.each do |x|
-  input.each do |y|
-    
-    if x > y and input.index(x) < input.index(y) then
-      if not swaps.has_key?(x)
-        swaps[input.index(x)] = input.index(y)
-      end
-    end
-  end
-end
-
-swaps.each do |x|
-  swap x, swaps[x], input
-end
-
-puts input
