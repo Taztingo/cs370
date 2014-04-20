2713cf27d67f47b4251d7d90e32c4b8eadb25114
Andy <kooshaba@gmail.com>
2014-04-20 09:12:41 -0400

    still trying

--- baler/eeny_meeny/main.rb
+++ baler/eeny_meeny/main.rb
@@ -1,42 +1,81 @@
 candidates = gets.chomp.to_i
-remove_pos = gets.chomp.to_i
+candidates_max = gets.chomp.to_i
 
-# Fill array with Xs denoting people
-positions = Array.new(candidates) { |x| x = 'X'}
+# Hard coded value of index of the 'o' in Eeny...
+remove_pos = 15
 
-# Increment at which people are removed
-pos = remove_pos
 
-# The closest position to the big guy
-min_pos = candidates
+mins = []
+chiefs = []
 
-# While the chief hasn't been chosen
-while positions.count('X') != 1 do
+# Candidate = Struct.new(:original_position, :)
 
-  #If the position is one to be chosen
-  if(pos % remove_pos == 0)
+#do once for every estimation
+(candidates..candidates_max).each do |i|
 
-    # If the position is empty move on until you find a person
-    while positions[pos] == '_'
+  # Fill array with Xs denoting people
+  positions = Array.new(i) { |x| x = 'X'}
+  puts "number of people #{i}"
+  print positions
+  gets
+
+  pos = -1
+
+  # Determine if someone is kicked out yet
+  eeny = 0
+
+  # The closest position to the big guy
+  min_pos = i
+
+
+  # While the chief hasn't been chosen
+  while positions.count('X') != 1 do
+
+    #If the position is one to be chosen
+    if(eeny == 15)
+      eeny = 0
+
+      # If the position is empty move on until you find a person
+      while positions[pos] == '_'
+        puts "Finding new pos, #{pos} was taken"
+        pos = pos + 1
+      end
+
+      puts "Current Pos: #{pos}"
+      # Remove person found
+      # print positions
+      positions[pos] = '_'
+
+      # If a new closest pos was found
+      if(pos < min_pos && pos != 0)
+        min_pos = pos
+      end
+    end
+
+    # Loop around to front of circle
+    if(pos + 1 > positions.length)
+        pos = 0
+    else
       pos = pos + 1
     end
 
-    puts pos
-    # Remove person found
-    positions[pos] = '_'
-
-    # If a new closest pos was found
-    if(pos < min_pos && pos != 0)
-      min_pos = pos
-    end
+    eeny = eeny + 1
   end
 
-  # Loop around to front of circle
-  if(pos + 1 >= positions.length)
-      pos = 0
-  else
-    pos = pos + 1
-  end
+  puts "Min #{min_pos}"
+  mins << min_pos
+  puts "Chief #{positions.index('X')}"
+  chiefs << positions.index('X')
+
 end
 
-puts min_pos
+print chiefs
+puts
+
+for x in mins
+  puts "Checking #{x} in chiefs"
+  if(chiefs.contains?(x))
+    puts x
+    break
+  end
+end
